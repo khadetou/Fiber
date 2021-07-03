221539edce9f8808b7ab5dffbaada2dafbea2fdc
khadetou <khadetou96@gmail.com>
2021-07-03 09:38:54 +0000

    header

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,43 +1,61 @@
 import Styles from '../styles/style.module.scss'
+import { useState } from 'react'
+import { HiMenu } from 'react-icons/hi'
 const Navbar = () => {
   const {
+    show,
+    wrapper,
     header,
     header__logo,
     header__logoText,
     header__headerContent,
     header__navigation,
     header__ul,
+    header__li,
     header__auth,
     header__signIn,
     header__signUp,
+    header__menu,
   } = Styles
+
+  const [dropDown, setDropDown] = useState(false)
 
   return (
     <div className={header}>
-      <div className={header__headerContent}>
-        <div className={header__logo}>
-          <h1 className={header__logoText}>Fiber</h1>
-        </div>
-        <div className={header__navigation}>
-          <ul className={header__ul}>
-            <li>
-              <a href="#">Comunity</a>
-            </li>
-            <li>
-              <a href="#">Pricing</a>
-            </li>
-            <li>
-              <a href="#">Features</a>
-            </li>
-          </ul>
-        </div>
-        <div className={header__auth}>
-          <a className={header__signIn} href="#">
-            Sign In
-          </a>
-          <a className={header__signUp} href="#">
-            Sign Up
-          </a>
+      <div className={wrapper}>
+        <div className={header__headerContent}>
+          <div className={header__logo}>
+            <h1 className={header__logoText}>Fiber</h1>
+          </div>
+          <div
+            className={
+              dropDown ? `${header__navigation} ${show}` : header__navigation
+            }
+          >
+            <ul className={header__ul}>
+              <li className={header__li}>
+                <a href="#">Comunity</a>
+              </li>
+              <li className={header__li}>
+                <a href="#">Pricing</a>
+              </li>
+              <li className={header__li}>
+                <a href="#">Features</a>
+              </li>
+            </ul>
+          </div>
+          <div className={dropDown ? `${header__auth} ${show}` : header__auth}>
+            <a className={header__signIn} href="#">
+              Sign In
+            </a>
+            <a className={header__signUp} href="#">
+              Sign Up
+            </a>
+          </div>
+          <HiMenu
+            className={header__menu}
+            onClick={() => setDropDown(!dropDown)}
+          />
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import styles from '../styles/style.module.scss'
 export default function Home() {
   const { h1 } = styles
-  return (
-    <div>
-      <h1 className={h1}>Hello world</h1>
-    </div>
-  )
+  return <div></div>
 }
